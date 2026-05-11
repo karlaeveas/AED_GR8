6342b2f2f0c5d92bf42d9a63a6c9c0761274d9a1
karlaeveas <164652971+karlaeveas@users.noreply.github.com>
2026-05-11 07:21:24 -0500

    Delete bstreeInterface directory

--- bstreeInterface/BinarySearchTree.java
+++
@@ -1,9 +0,0 @@
-package bstreeInterface;
-
-public interface BinarySearchTree<E extends Comparable<E>> {
-
-    void insert(E data) throws Exception;
-    E search(E data) throws Exception;
-    void delete(E data) throws Exception;
-    boolean isEmpty();
-}
